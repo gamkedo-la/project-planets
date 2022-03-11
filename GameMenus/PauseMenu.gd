a82c26e605b38ffd5b3512aeb5255fc62b774bfb
Simon Donohoe <simon.donohoe@gmail.com>
2022-03-11 13:30:05 +0000

    Added a simple pause menu

---
+++ GameMenus/PauseMenu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+		$Title/BottomHalf_PauseMenu/ResumeBtn.grab_focus()
+		
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+ 
+func _on_ResumeBtn_pressed():
+	print("Resume Button pressed")
+	self.is_paused = false
+	
+func _on_RestartBtn_pressed():
+	print("Restart Button pressed")
+	get_tree().change_scene("res://Main.tscn")
+	self.is_paused = false
+
+func _on_QuitBtn_pressed():
+	print("Quit Button pressed")
+	get_tree().change_scene("res://GameMenus/StartMenu.tscn")
+	self.is_paused = false
+
+func _on_ExitBtn_pressed():
+	print("Exit Button pressed")
+	get_tree().quit()
--- UI/Controls/PauseGame.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _input(event):
-	if event.is_action_pressed("pause"):
-		var pause_game = not get_tree().paused
-		get_tree().paused = pause_game
-		visible = pause_game
