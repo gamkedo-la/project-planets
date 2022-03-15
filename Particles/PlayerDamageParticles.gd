8e25f377e1093c5f2d493c1530204bfd53a8e27d
Christer Kaitila <breakdance@mcfunkypants.com>
2022-03-15 11:29:20 -0700

    player emits explosion SFX and damage particles when hit

--- Particles/PlayerDamageParticles.gd
+++ Particles/PlayerDamageParticles.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# FIXME why does this never get triggered? =(
 func _on_Timer_timeout():
 	queue_free()
