f794c0b613a460985d8fa5af7da088d24fd80c46
Gabriel Cornish <58050002+gabrielcornish@users.noreply.github.com>
2022-03-16 08:23:06 -0700

    Fixed bug where player explosions fired continuously.

--- Particles/PlayerDamageParticles.gd
+++ Particles/PlayerDamageParticles.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# $Timer.start()
 	pass # Replace with function body.
 
-# FIXME why does this never get triggered? =(
+
+
 func _on_Timer_timeout():
 	queue_free()
