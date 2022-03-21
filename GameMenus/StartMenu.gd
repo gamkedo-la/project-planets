ea1579cc00b1853add3ab736265b53d9293c9eba
Gabriel Cornish <58050002+gabrielcornish@users.noreply.github.com>
2022-03-21 08:35:39 -0700

    Created first 3 levels

--- GameMenus/StartMenu.gd
+++ GameMenus/StartMenu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func _on_StartBtn_pressed():
 	print("Start Button pressed")
-	get_tree().change_scene("res://Main.tscn")
+	get_tree().change_scene("res://Levels/Level_1.tscn")
 
 
 func _on_OptionsBtn_pressed():
