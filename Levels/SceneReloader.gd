12ba72599e860f90aa38e83dfda9363731cecb32
Simon Donohoe <simon.donohoe@gmail.com>
2022-03-23 13:13:36 +0000

    Added a player score reset when restarting game

--- Levels/SceneReloader.gd
+++ Levels/SceneReloader.gd
@@ -5,4 +5,6 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("reload"):
+		if Global.orbs_collected > 0:
+			Global.orbs_collected = 0
 		get_tree().reload_current_scene()
--- UI/Objective.gd
+++ UI/Objective.gd
@@ -4,6 +4,7 @@ var score
 
 func _ready():
 	score = 0
+	print("Score is " + str(score))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +14,5 @@ func _process(delta):
 		score = Global.orbs_collected
 		$AnimationPlayer.play("score_increase")
 		$Control/ObjectiveRequirement.text = str(score) + " / 100"
+		print("Score is " + $Control/ObjectiveRequirement.text)
 #	pass
