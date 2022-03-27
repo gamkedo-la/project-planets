ddee2633a3d88ca9a28b336cf11ea9468e34eaec
Simon Donohoe <simon.donohoe@gmail.com>
2022-03-27 22:30:03 +0100

    Added a little animation to dialogue box

--- Dialogue/PlayerDialogue.gd
+++ Dialogue/PlayerDialogue.gd
@@ -1,32 +1,53 @@
 extends CanvasLayer
 
 export(String, FILE, "*.json") var dialogue_file
+export(float) var dialogSpeed = 0.05
 
 var dialogues = []
+
 var current_dialogue_id = 0
+var finished = false
+
 
 func _ready():
+	$DialogUI/Timer.wait_time = dialogSpeed
 	play()
 	
 func play():
 	dialogues = load_dialogue()
 	
-	current_dialogue_id = -1
+	#current_dialogue_id = -1
 	next_line()
 	
 func _input(event):
+	#var endOfConversation = "res://Dialogue/" + dialogues[current_dialogue_id]["fileName"]
+	
 	if event.is_action_pressed("ui_accept"):
-		next_line()
+		"""
+		print("res://Dialogue/" + dialogues[current_dialogue_id]["fileName"])
+		if endOfConversation == "res://Dialogue/Level2":
+			queue_free()
+			Global.goto_scene("res://Levels/Level_1.tscn")
+		"""
+			
+		if finished:
+			next_line()
+		else:
+			$DialogUI/DialogBox/Dialogue.visible_characters = len($DialogUI/DialogBox/Dialogue.text)
+			dialogue_indicator()
 		
 func next_line():
 	print(current_dialogue_id)
-	current_dialogue_id += 1
+	print(Global.current_scene)
+	$DialogUI/DialogBox/Indicator.visible = false
 	
 	if current_dialogue_id >= len(dialogues):
 		print("End of dialogue")
 		queue_free()
 		Global.goto_scene("res://Levels/Level_1.tscn")
 		return
+		
+	finished = false
 		
 	var file = File.new()
 	var img = "res://Dialogue/" + dialogues[current_dialogue_id]["fileName"] + ".png"
@@ -38,9 +59,25 @@ func next_line():
 	$DialogUI/DialogBox/Name.text = dialogues[current_dialogue_id]["name"]	
 	$DialogUI/DialogBox/Dialogue.text = dialogues[current_dialogue_id]["text"]
 	
+	$DialogUI/DialogBox/Dialogue.visible_characters = -1
+	
+	while $DialogUI/DialogBox/Dialogue.visible_characters < len($DialogUI/DialogBox/Dialogue.text):
+		$DialogUI/DialogBox/Dialogue.visible_characters += 1
+		
+		$DialogUI/Timer.start()
+		yield($DialogUI/Timer, "timeout")
+	finished = true
+	dialogue_indicator()
+	current_dialogue_id += 1
+	return
+	
 func load_dialogue():
 	var file = File.new()
 	
 	if file.file_exists(dialogue_file):
 		file.open(dialogue_file, file.READ)
-		return parse_json(file.get_as_text())
+		return parse_json(file.get_as_text())		
+
+func dialogue_indicator():
+	$DialogUI/DialogBox/Indicator.visible = true
+	$DialogUI/DialogBox/Indicator/AnimationPlayer.play("Bounce")
