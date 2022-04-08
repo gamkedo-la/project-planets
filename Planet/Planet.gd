b31751df432e87ea060956e65dbf4fbb8d4ac4e5
Christer Kaitila <breakdance@mcfunkypants.com>
2022-04-08 13:10:20 -0700

    healthpacks spawn 5% of the time instead of ore

--- Planet/Planet.gd
+++ Planet/Planet.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
 export (PackedScene) var ore
-
-# FIXME:
-# export (PackedScene) var Healthpack # how the heck do you set this to a value?
-
+export (PackedScene) var Healthpack # how the heck do you set this to a value?
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +10,13 @@ func _ready():
 
 func spawn_reward(pos):
 	
-	var o = ore.instance() #usually
+	var o
 	
-# FIXME:
-#	if rand_range(1,100)>90: # rarely spawn a healthpack
-#		o = Healthpack.instance()
-	
+	if rand_range(1,100)>95: # rarely spawn a healthpack
+		o = Healthpack.instance()
+	else: #usually spawn some ore
+		o = ore.instance() 
+
 	call_deferred("add_reward_to_scene",o)
 	#Global.current_scene.add_child(o)
 	#Global.call_deferred("add_child",o)
