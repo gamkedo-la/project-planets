9965f770456951ae4af9caeb31df576df28f3a14
Simon Donohoe <simon.donohoe@gmail.com>
2022-04-16 11:24:55 +0100

    Orb counter reset to display player score properly
    After I added a fade in to each level the orb counter went missing from levels after level 1.

--- Global/Global.gd
+++ Global/Global.gd
@@ -34,6 +34,8 @@ func _deferred_goto_scene(path):
 	
 	current_scene = s.instance()
 	
+	reset_orb_count()
+	
 	get_tree().get_root().add_child(current_scene)
 	
 	#get_tree().set_current_scene(current_scene)
