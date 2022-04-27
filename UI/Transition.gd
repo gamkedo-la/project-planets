2d1939577ba8703e92288ba464f25aeba977a87d
Simon Donohoe <simon.donohoe@gmail.com>
2022-04-27 12:51:39 +0100

    Disabled fade animation, was causing an error

--- UI/Transition.gd
+++ UI/Transition.gd
@@ -9,15 +9,15 @@ func _ready():
 
 func transition():
 	$ColorRect.visible = true
-	anim.play("FadeIn")
+	#Global.reset_orb_count()
+	emit_signal("transitioned")
+	#anim.play("FadeIn")
 	print("fading out")
 	
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "FadeIn":
-		Global.reset_orb_count()
 		anim.play("FadeOut")
 		emit_signal("transitioned")
 	if anim_name == "FadeOut":
 		$ColorRect.visible = false
-		
