91a94f3cadb5915d59609026655e4cb74ebec342
gabrielcornish <58050002+gabrielcornish@users.noreply.github.com>
2022-04-30 11:03:28 -0700

    Added an input pause on the Start Level screen

--- GameMenus/StartLevel.gd
+++ GameMenus/StartLevel.gd
@@ -1,13 +1,14 @@
 extends Control
 
 onready var only_once : bool = true
+var ready_for_input = false
 
 func _ready():
 	get_tree().paused = true
 	$TopHalf/LevelNameLabel.text = "Level " + str(Global.level_number)
 	
 func _input(event):
-	if event is InputEventKey:
+	if event is InputEventKey && ready_for_input:
 		if event.pressed && only_once:
 			var start_level_pause = not get_tree().paused
 			get_tree().paused = start_level_pause
@@ -16,3 +17,8 @@ func _input(event):
 			$AudioStreamPlayer.playing = false
 
 
+
+
+func _on_Timer_timeout():
+	ready_for_input = true
+	pass # Replace with function body.
